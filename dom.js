9af2cf7145b68a4a78ffceb4cd0c1e98bba3108d
ddeo1301 <divyanshudeo1301@gmail.com>
2023-02-04 16:37:29 +0530

    Expense Tracker

---
+++ dom.js
@@ -0,0 +1,49 @@
+function registerUser(event){
+    event.preventDefault();
+
+    const expense = event.target.expense.value;
+    const description = event.target.description.value;
+    const category = event.target.category.value;
+
+    const obj = {
+        expense,
+        description,
+        category
+    }
+
+    localStorage.setItem(expense, JSON.stringify(obj));
+    showUserOnScreen(obj);
+
+    function showUserOnScreen(obj){
+        const parentItem = document.getElementById("list of expense");
+        const childItem = document.createElement('li');
+
+        childItem.textContent = obj.expense + "-" + obj.description + "-" + obj.category;
+        parentItem.appendChild(childItem);
+
+        const deleteItem = document.createElement('input');
+        deleteItem.type = 'button';
+        deleteItem.value = 'DELETE';
+        deleteItem.onclick = () => {
+            localStorage.removeItem(obj.expense);
+            parentItem.removeChild(childItem);
+        }
+
+        const editItem = document.createElement('input');
+        editItem.type = 'button';
+        editItem.value = 'EDIT';
+        editItem.onclick = () => {
+            localStorage.removeItem(obj.expense);
+            parentItem.removeChild(childItem);
+
+            document.getElementById('expense').value = obj.expense;
+            document.getElementById('description').value = obj.description;
+            document.getElementById('category').value = obj.category;
+        }
+
+
+        parentItem.appendChild(childItem);
+        parentItem.appendChild(deleteItem);
+        parentItem.appendChild(editItem);
+    }
+}
